db6013a2ca33d71cb81324d1011390bd8d8725e6
Tula Ram Subba <tularamsubba@Tulas-MacBook-Air.local>
2017-01-19 21:04:06 -0500

    hwarray

--- homework.rb
+++ homework.rb
@@ -1,3 +1,3 @@
 def create_1_to_100
-    array=* (1..100)
+   array=* (1..100)
 end
--- minedmindskata2.rb
+++ minedmindskata2.rb
@@ -5,9 +5,11 @@ require_relative "homework.rb"
 class TestMindMindkata < Minitest::Test
 
    def test_1_return_1
-   	result = create_1_to_100()
-   	 assert_equal(100,result.count)
- end
-   def test_3_equals_mined
+   	results = create_1_to_100()
+   	 assert_equal(100,results.count)
+  end
+  def test_array_100_elements
+  	results = create_1_to_100()
+  	 assert_equal(100,results.count())
   end
 end
